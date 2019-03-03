aec378b5c5a5315fcfa697e7df03d58a99336b9b
Aiden Chia <aidenchia95@gmail.com>
2019-03-03 14:23:45 +0800

    fix bugs

--- set_env_var.sh
+++ set_env_var.sh
@@ -1,5 +1,4 @@
 export DATABASE_URL=$(heroku config:get DATABASE_URL -a evening-forest-28744)
 export APP_SETTINGS="config.ProductionConfig"
 export PYTEST_ADDOPTS="-v --disable-warnings"
-export LOCAL_DATABASE_URL="postgresql://localhost/mylocaldb"
 export DATABASE_NAME="postgresql-polished-71084"
