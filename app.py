0b828788bc30bf4e3059c9bd9fc90c5132824504
Aiden Chia <aidenchia95@gmail.com>
2019-04-03 10:44:38 +0800

    removed the second @app.route

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ def Roles(included=True, *role):
  
 ########################################## ALL USERS ##########################
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/login', methods=['GET','POST'])
 def login():
   if current_user.is_authenticated:
     return redirect(url_for('courseInput'))
