1f30df38803d82d25a80bfc82a5cf39f0d970575
Aiden Chia <aidenchia95@gmail.com>
2019-04-05 16:15:05 +0800

    adding compile step because of different Java Runtime

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def Roles(included=True, *role):
 @app.route('/', methods=['GET', 'POST'])
 def login():
   import subprocess
+  subprocess.call(['javac', 'Scheduler.java'])
   subprocess.call(['java', 'Scheduler'])
 
   if current_user.is_authenticated:
