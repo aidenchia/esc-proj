42e1935ee2f5cf4766c5597aae43d229fa3e327b
Aiden Chia <aidenchia95@gmail.com>
2019-04-05 15:14:17 +0800

    removing java subprocess

--- app.py
+++ app.py
@@ -47,8 +47,9 @@ def Roles(included=True, *role):
 ########################################## ALL USERS ##########################
 @app.route('/', methods=['GET', 'POST'])
 def login():
-  import subprocess
-  subprocess.call(['java'], ['Scheduler'])
+  #import subprocess
+  #subprocess.call(['java'], ['Scheduler'])
+
   if current_user.is_authenticated:
     return redirect(url_for('courseInput'))
 
