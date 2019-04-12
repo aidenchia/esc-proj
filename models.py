70b2b5c2a0c97b86d08860464a84d98e260b3bf7
RealTanYou <43871431+RealTanYou@users.noreply.github.com>
2019-04-12 16:34:22 +0800

    Update models.py

--- models.py
+++ models.py
@@ -88,8 +88,8 @@ class Users(db.Model):
   #subject = db.Column(db.String, nullable=True)
   
   # Prof - specific fields
-  professor_id = db.Column(db.Integer, nullable=True)
-  coursetable = db.Column(db.String, nullable=True)
+  #professor_id = db.Column(db.Integer, nullable=True)
+  #coursetable = db.Column(db.String, nullable=True)
 
 
   def __init__(self, username, fullname, email, password, user_group, authenticated):
