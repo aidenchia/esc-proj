4c40306dee702357895db743f6b2f1641556df8b
RealTanYou <43871431+RealTanYou@users.noreply.github.com>
2019-04-17 09:19:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,8 @@ def register():
 
     flash('Invalid Parameters')
   print("came herer from register")
-  return render_template('register.html',form=form)
+  return render_template('addRooms.html')
+  #return render_template('register.html',form=form)
 
 @app.route("/subjects", methods=['GET', 'POST'])
 @Roles(True,"admin", "course_lead", "pillar_head")
